46f51e034ea87e5acf71a21e43114cc70e254095
sparsh badjate <sparshbadjate27@gmail.com>
2023-07-21 23:32:00 +0530

    adding updated second process fie

--- week-2/code_along/secondProcess.js
+++ week-2/code_along/secondProcess.js
@@ -10,3 +10,8 @@ function callbackFn(result){
 }
 
 fetch("http://localhost:3000/queryHandleSum?counter=100", sendObj).then(callbackFn) 
+
+
+// fetch is a libraray used to establish communication between two node js process basically helps us communicate between two http server the first argument is the url + route followed by the object and this returns a promis 
+
+//result.json is a also a function which helps us extract the json from the resukt and returns a promise 
